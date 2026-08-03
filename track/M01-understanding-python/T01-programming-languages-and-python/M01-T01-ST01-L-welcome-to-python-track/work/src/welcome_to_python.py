b9143a5404bd9cd13642544dce86ff0291afef7a
unknown <chaitanyavarmat@gmail.com>
2026-08-03 17:50:01 +0530

    push

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
@@ -1,1 +1,14 @@
+#hello world 
 print("hello world")
+# print simple message
+print("I am learning python")
+print("python is fun")
+# variable and data types
+Student_name="arrav"
+Course_duration=30
+Course_rating=4.8
+is_enrolled=True
+print(type(Student_name))
+print(type(Course_duration))
+print(type(Course_rating))
+print(type(is_enrolled))
