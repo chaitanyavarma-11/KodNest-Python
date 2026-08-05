e29a379da8d1684f64b548ff21fb001cb98ad06b
chaitanyavarma-11 <chaitanyavarmat@gmail.com>
2026-08-05 10:18:23 +0530

    push

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/convert_to-numbers-to-strings.py
@@ -0,0 +1,19 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+# convert the values here
+age=int(age_text)
+course_fee=float(course_fee_text)
+attempt_text=str(attempts)
+#Display the converted values and their data types
+print(age)
+print(type(age))
+print(course_fee)
+print(type(course_fee))
+print(attempt_text)
+print(type (attempt_text))
+
+
+
+
+
