36681114af489a294519942d4acc6b3609fc1e17
chaitanyavarma-11 <chaitanyavarmat@gmail.com>
2026-08-05 11:02:32 +0530

    push

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/m02/t02/src/division_vs_floordivision.py
@@ -0,0 +1,5 @@
+first_number = int(input())
+second_number = int(input())
+# write your code here
+print(first_number/second_number)
+print(first_number//second_number)
---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/m02/t02/src/variable&type().py
@@ -0,0 +1,12 @@
+# Create the required variables
+# Display the data type of each variable
+Student_name="Arrav"
+Course_duration=30
+Course_rating=4.8
+is_enrolled=True
+print(type (Student_name))
+print(type (Course_duration))
+print(type (Course_rating))
+print(type(is_enrolled))
+
+
