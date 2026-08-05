c0bae530733199590bb61ae1b7c99663f666ed89
chaitanyavarma-11 <chaitanyavarmat@gmail.com>
2026-08-05 18:34:21 +0530

    push

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/m02/t02/src/check positive,negative or zero.py
@@ -0,0 +1,12 @@
+# Read the number
+Number =int(input())
+# check wheather the number is positive, negative or zero
+if Number==0:
+  print("Number is Zero")
+elif Number<0:
+    print("Number is Negative")
+else:
+    print("Number is Positive")
+
+
+
