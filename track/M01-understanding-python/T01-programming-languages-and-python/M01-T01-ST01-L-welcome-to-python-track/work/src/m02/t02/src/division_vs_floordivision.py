f14f9f2e28c05166bf91c7e3ba1997cb0dc33060
chaitanyavarma-11 <chaitanyavarmat@gmail.com>
2026-08-05 13:48:28 +0530

    push

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/m02/t02/src/division_vs_floordivision.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/m02/t02/src/division_vs_floordivision.py
@@ -1,5 +1,5 @@
-first_number = int(input())
-second_number = int(input())
+first_number = int(input("enter number one"))
+second_number = int(input("enter number two"))
 # write your code here
 print(first_number/second_number)
 print(first_number//second_number)
---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/m02/t02/src/pseudocode.py
@@ -0,0 +1,11 @@
+Studentname=input("enter")
+StudentAge=int(input("enter age"))
+Courserating=float(input("enter rating"))
+# Display the values
+print(f"Student: {Studentname}")
+print(f"Age: {StudentAge}")
+print(f"Rating:{Courserating}")
+# Display the data types
+print(type(Studentname))
+print(type(StudentAge))
+print(type(Courserating))
