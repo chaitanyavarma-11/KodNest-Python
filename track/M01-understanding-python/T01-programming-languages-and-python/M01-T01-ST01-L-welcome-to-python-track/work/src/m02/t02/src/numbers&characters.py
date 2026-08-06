303e481b52ec7d4c16fc785f0cdcb3c7095ff347
chaitanyavarma-11 <chaitanyavarmat@gmail.com>
2026-08-06 16:39:14 +0530

    push

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/m02/t02/src/numbers&characters.py
@@ -0,0 +1,9 @@
+# Read the number and word
+n = int(input())
+word = input()
+#Print the number sequence
+print ("Numbers:")  
+for i in range(1,n+1):
+    print(i) 
+for ch in word:
+    print("charecters",ch)
